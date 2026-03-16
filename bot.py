5aaac12e90a88f47744760e7fce87d61e633d864
Qwizz-qiz <niknamesuper@gmail.com>
2026-03-16 18:32:40 +0100

    Fix chat ID reference in message forwarding

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 TOKEN = "8532195297:AAFM92ig8QBj_McFR8d-37Pw0HZgor6lxDQ"
-GROUP_ID =  -1003426813973
+GROUP_ID = -1003426813973
 
 async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message:
         await context.bot.forward_message(
             chat_id=GROUP_ID,
-            from_chat_id=update.message.chat_id,
+            from_chat_id=update.effective_chat.id,
             message_id=update.message.message_id
         )
 
