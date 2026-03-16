878898f25bdf776dee42728c45613dba3c65e886
Qwizz-qiz <niknamesuper@gmail.com>
2026-03-16 17:56:20 +0100

    Fix TOKEN variable format in bot.py
    Updated the TOKEN variable to be a string.

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = AAFM92ig8QBj_McFR8d-37Pw0HZgor6lxDQ
+TOKEN = "8532195297:AAFM92ig8QBj_McFR8d-37Pw0HZgor6lxDQ"
 GROUP_ID = -5225412229
 
 async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
